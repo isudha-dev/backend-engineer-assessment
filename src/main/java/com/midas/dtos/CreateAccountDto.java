4add87db1b0a3d67ef68d03b509c272b146dab08
Sudha Maurya <sudha@Sudhas-MacBook-Pro.local>
2024-02-06 16:14:27 +0530

    created impl files and added enums

--- src/main/java/com/midas/dtos/CreateAccountDto.java
+++ src/main/java/com/midas/dtos/CreateAccountDto.java
@@ -9,11 +9,8 @@ import java.util.UUID;
 @Setter
 @Getter
 public class CreateAccountDto {
-
     private UUID id;
     private String firstName;
-
     private String lastName;
-
     private String email;
 }
